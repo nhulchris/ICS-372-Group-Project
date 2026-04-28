72fd88da43677767d4e2568f66924bd4f3205972
nhulchris <nhul.chris@gmail.com>
2026-04-28 01:13:56 -0500

    Update BaristaObserver.java

--- src/main/java/com/brewbite/observer/BaristaObserver.java
+++ src/main/java/com/brewbite/observer/BaristaObserver.java
@@ -1,21 +1,9 @@
 package com.brewbite.observer;
 
-import com.brewbite.controller.BaristaController;
-
 public class BaristaObserver implements Observer {
-
-    private BaristaController controller;
-
-    public BaristaObserver(BaristaController controller) {
-        this.controller = controller;
-    }
 
     @Override
     public void update(String message) {
         System.out.println("Barista notified: " + message);
-
-        if (controller != null) {
-            controller.addOrderToView("New Order Received");
-        }
     }
 }
