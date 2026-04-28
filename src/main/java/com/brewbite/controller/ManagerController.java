3e85678a672c01889fdc333ec180b9f0fd0756a7
nhulchris <nhul.chris@gmail.com>
2026-04-28 01:12:21 -0500

    Create ManagerController.java

---
+++ src/main/java/com/brewbite/controller/ManagerController.java
@@ -0,0 +1,11 @@
+package com.brewbite.controller;
+
+import javafx.fxml.FXML;
+
+public class ManagerController {
+
+    @FXML
+    public void handleRestock() {
+        System.out.println("Inventory restocked.");
+    }
+}
