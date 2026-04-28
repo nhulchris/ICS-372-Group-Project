27e23cccc11be5a0fb226cf271c0545f8cad9fb3
nhulchris <nhul.chris@gmail.com>
2026-04-28 01:23:37 -0500

    Update ManagerController.java

--- src/main/java/com/brewbite/controller/ManagerController.java
+++ src/main/java/com/brewbite/controller/ManagerController.java
@@ -1,5 +1,10 @@
 package com.brewbite.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class ManagerController {
@@ -9,3 +14,15 @@ public class ManagerController {
         System.out.println("Inventory restocked.");
     }
 }
+
+    @FXML
+    public void goBack(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/role-selection.fxml"));
+            Scene scene = new Scene(loader.load(), 600, 400);
+            stage.setScene(scene);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
