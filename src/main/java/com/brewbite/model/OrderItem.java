a41437e8d63ae2ac601dc17e3e9bfbbbf266e686
nhulchris <nhul.chris@gmail.com>
2026-04-27 23:32:20 -0500

    Create OrderItem.java

---
+++ src/main/java/com/brewbite/model/OrderItem.java
@@ -0,0 +1,16 @@
+package com.brewbite.model;
+
+public class OrderItem {
+
+    private MenuItem menuItem;
+    private int quantity;
+
+    public OrderItem(MenuItem menuItem, int quantity) {
+        this.menuItem = menuItem;
+        this.quantity = quantity;
+    }
+
+    public double getTotalPrice() {
+        return menuItem.getPrice() * quantity;
+    }
+}
