4a99029b77ca7e619b48a6735db5bf48528374a0
nhulchris <nhul.chris@gmail.com>
2026-04-27 23:38:46 -0500

    Create Subject.java

---
+++ src/main/java/com/brewbite/observer/Subject.java
@@ -0,0 +1,10 @@
+package com.brewbite.observer;
+
+public interface Subject {
+
+    void addObserver(Observer observer);
+
+    void removeObserver(Observer observer);
+
+    void notifyObservers(String message);
+}
