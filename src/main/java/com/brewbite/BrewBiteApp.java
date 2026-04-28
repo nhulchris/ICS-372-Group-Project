06ac38b6ab8cc7fc57d41dc556482ce3a01df963
nhulchris <nhul.chris@gmail.com>
2026-04-28 01:11:55 -0500

    Update BrewBiteApp.java

--- src/main/java/com/brewbite/BrewBiteApp.java
+++ src/main/java/com/brewbite/BrewBiteApp.java
@@ -6,16 +6,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 public class BrewBiteApp extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/customer-view.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/role-selection.fxml"));
             Scene scene = new Scene(loader.load(), 600, 400);
-
+    
             primaryStage.setTitle("Brew & Bite Cafe System");
             primaryStage.setScene(scene);
             primaryStage.show();
+    
         } catch (Exception e) {
             e.printStackTrace();
         }
