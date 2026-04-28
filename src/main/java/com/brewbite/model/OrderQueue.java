ea8216233c15c7a599e2aa6ac9307c78cd38f6f6
nhulchris <nhul.chris@gmail.com>
2026-04-27 23:39:21 -0500

    Update OrderQueue.java

--- src/main/java/com/brewbite/model/OrderQueue.java
+++ src/main/java/com/brewbite/model/OrderQueue.java
@@ -1,17 +1,41 @@
 package com.brewbite.model;
 
+import com.brewbite.observer.Observer;
+import com.brewbite.observer.Subject;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
-public class OrderQueue {
+public class OrderQueue implements Subject {
 
     private Queue<Order> orders = new LinkedList<>();
+    private List<Observer> observers = new ArrayList<>();
 
     public void addOrder(Order order) {
         orders.add(order);
+        notifyObservers("New order added");
     }
 
     public Order getNextOrder() {
         return orders.poll();
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
     }
 }
