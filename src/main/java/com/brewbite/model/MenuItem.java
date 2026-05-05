c4e2203226bd9bd9674f8c7b8ab4624f40130a07
nhulchris <nhul.chris@gmail.com>
2026-05-05 11:55:34 -0500

    Improve MenuItem: add setters for modify support, add equals/hashCode, rename getPrice to getBasePrice

--- src/main/java/com/brewbite/model/MenuItem.java
+++ src/main/java/com/brewbite/model/MenuItem.java
@@ -1,7 +1,15 @@
 package com.brewbite.model;
 
 import java.util.Map;
+import java.util.Objects;
 
+/**
+ * Abstract base class for items that can appear on the menu.
+ * Subclasses (Beverage, Pastry) define how the price is calculated.
+ *
+ * Demonstrates Inheritance: Beverage and Pastry share name, basePrice,
+ * and ingredient consumption while specializing pricing behavior.
+ */
 public abstract class MenuItem {
 
     protected String name;
@@ -18,18 +26,46 @@ public abstract class MenuItem {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
     public Map<String, Integer> getIngredients() {
         return ingredients;
     }
 
-    public double getPrice() {
-        return basePrice;
+    public void setIngredients(Map<String, Integer> ingredients) {
+        this.ingredients = ingredients;
     }
+
+    /**
+     * Polymorphic price calculation overridden by each subclass.
+     */
+    public abstract double calculatePrice();
 
     @Override
     public String toString() {
         return name + " - $" + String.format("%.2f", calculatePrice());
     }
 
-    public abstract double calculatePrice();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MenuItem that)) return false;
+        return Double.compare(that.basePrice, basePrice) == 0
+                && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, basePrice);
+    }
 }
